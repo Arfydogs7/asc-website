54220685bf2db2319f0de828f2d3e48b4a7419f3
Elaine Chao <elaine.iy@outlook.com>
2020-10-20 17:28:02 -0700

    added announcement routes

--- routes/announcements.js
+++ routes/announcements.js
@@ -17,12 +17,12 @@ router.get("/", async(req, res) => {
 })
 
 //new announcement
-router.get("/new", (req, res) => {
+router.get("/new", ensureAuth, (req, res) => {
     res.render("announcements/new", { announcement: new Announcement() })
 })
 
 //create announcement
-router.post("/", async(req, res) => {
+router.post("/", ensureAuth, async(req, res) => {
     const announcement = new Announcement({
         title: req.body.title,
     })
@@ -38,9 +38,51 @@ router.post("/", async(req, res) => {
 })
 
 //edit announcement
+router.get("/:id/edit", ensureAuth, async(req, res) => {
+    try {
+        const announcement = await Announcement.findById(req.params.id)
+        res.render("announcements/edit", { announcement: announcement })
+    } catch {
+        res.redirect("/login")
+    }
+})
 
 //update announcement
+router.put("/:id", async(req, res) => {
+    let announcement;
+    try {
+        announcement = await Announcement.findById(req.params.id)
+        announcement.title = req.body.title,
+            await announcement.save()
+        res.redirect(`/announcements`)
+    } catch {
+        if (!announcement) {
+            res.redirect("/login")
+        } else {
+            res.render("announcements/edit", {
+                announcement: announcement,
+                errorMessage: "error updating announcement"
+            })
+        }
+    }
+})
 
 //delete announcement
+router.delete("/:id", async(req, res) => {
+    let announcement;
+    try {
+        announcement = await Announcement.findById(req.params.id)
+        await announcement.remove()
+        res.redirect("/login")
+    } catch {
+        if (!announcement) {
+            res.redirect("/login")
+        } else {
+            res.render("/dashboard", {
+                errorMessage: "error deleting announcement"
+            })
+        }
+    }
+})
 
 module.exports = router
